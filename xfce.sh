f34f5d68bfa60200d2b548302de1cac98691d02b
Phil Hash <ht.phi99@gmail.com>
2025-06-04 00:06:20 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,8 +1,8 @@
-sudo apt install -y adb filezilla libxfce4ui-utils thunar xfce4-panel gnome-themes-extra \
+sudo apt install -y curl adb filezilla libxfce4ui-utils thunar xfce4-panel gnome-themes-extra \
 	xfce4-pulseaudio-plugin blueman gnome-disk-utility xfce4-session xfce4-settings \
 	fonts-recommended xfconf xfdesktop4 xfwm4 thunar-volman xfce4-notifyd \
 	xfce4-power-manager mousepad ristretto thunar-archive-plugin vlc atril \
-	xfce4-clipman-plugin ibus-unikey gammastep timeshift gh nodejs \
+	xfce4-clipman-plugin ibus-unikey gammastep timeshift gh \
 	xfce4-screenshooter xfce4-taskmanager xfce4-terminal xfce4-whiskermenu-plugin \
 	thunderbird flatpak gstreamer1.0-vaapi intel-media-va-driver-non-free \
 	network-manager-gnome i965-va-driver-shaders fastboot \
@@ -22,3 +22,5 @@ echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyri
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 #install vscodium
 sudo apt update && sudo apt install codium
+#install fnm
+curl -fsSL https://fnm.vercel.app/install | bash
